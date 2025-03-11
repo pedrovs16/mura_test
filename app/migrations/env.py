2261f82d28f2bba6b6c55a83c4e9ba6cc91b920e
Pedro Viana Schroeder <pedrovs.contato@gmail.com>
2025-03-11 19:09:05 -0300

    feat: add migration scripts and database configuration

---
+++ app/migrations/env.py
@@ -0,0 +1,124 @@
+from logging.config import fileConfig
+
+from alembic import context
+from sqlalchemy import create_engine, engine_from_config, pool
+
+from config import settings
+from infrastructure.db import Base
+from infrastructure.models import *  # noqa
+
+# this is the Alembic Config object, which provides
+# access to the values within the .ini file in use.
+config = context.config
+
+# Interpret the config file for Python logging.
+# This line sets up loggers basically.
+if config.config_file_name is not None:
+    fileConfig(config.config_file_name)
+
+target_metadata = Base.metadata
+
+url = (
+    f"postgresql+psycopg2://{settings.DB_USER}:{settings.DB_PASSWORD}"
+    f"@{settings.DB_HOST}:{settings.DB_PORT}/{settings.DB_NAME}"
+)
+
+config.set_main_option("sqlalchemy.url", url)
+
+print("config --- ", config.get_main_option("sqlalchemy.url"))
+
+
+host = (
+    f"postgresql+psycopg2://{settings.DB_USER}:{settings.DB_PASSWORD}"
+    f"@{settings.DB_HOST}:{settings.DB_PORT}/"
+)
+db_name = settings.DB_NAME
+
+engine = create_engine(host, isolation_level="AUTOCOMMIT")
+conn = engine.connect()
+
+existing_db = conn.execute(f"SELECT FROM pg_database WHERE datname = '{db_name}'").one_or_none()
+print("existing_db", existing_db)
+try:
+    if existing_db is None:
+        print(f"Ensuring database '{db_name}' exists on host '{host}'")
+        conn.execute(f"CREATE DATABASE {db_name}")
+except Exception as ex:
+    print(ex)
+finally:
+    conn.close()
+
+
+def include_object(object, name, type_, *args, **kwargs):
+    return not (type_ == "table")
+
+
+def run_migrations_offline() -> None:
+    """Run migrations in 'offline' mode.
+
+    This configures the context with just a URL
+    and not an Engine, though an Engine is acceptable
+    here as well.  By skipping the Engine creation
+    we don't even need a DBAPI to be available.
+
+    Calls to context.execute() here emit the given string to the
+    script output.
+
+    """
+
+    context.configure(
+        url=url,
+        target_metadata=target_metadata,
+        literal_binds=True,
+        dialect_opts={"paramstyle": "named"},
+        include_schemas=True,
+        compare_type=True,
+        compare_server_default=True,
+        include_object=include_object,
+    )
+
+    with context.begin_transaction():
+        context.run_migrations()
+
+
+def run_migrations_online() -> None:
+    """Run migrations in 'online' mode.
+
+    In this scenario we need to create an Engine
+    and associate a connection with the context.
+
+    """
+
+    # this callback is used to prevent an auto-migration from being generated
+    # when there are no changes to the schema
+    # reference: http://alembic.zzzcomputing.com/en/latest/cookbook.html
+    def process_revision_directives(context, revision, directives):
+        if getattr(config.cmd_opts, "autogenerate", False):
+            script = directives[0]
+            if script.upgrade_ops.is_empty():
+                directives[:] = []
+                print("No changes in schema detected.")
+
+    connectable = engine_from_config(
+        config.get_section(config.config_ini_section), prefix="sqlalchemy.", poolclass=pool.NullPool
+    )
+
+    with connectable.connect() as connection:
+        context.configure(
+            connection=connection,
+            target_metadata=target_metadata,
+            process_revision_directives=process_revision_directives,
+            include_schemas=True,
+            compare_type=True,
+            compare_server_default=True,
+            include_object=include_object,
+        )
+
+        with context.begin_transaction():
+            context.run_migrations()
+
+
+if context.is_offline_mode():
+    run_migrations_offline()
+else:
+    run_migrations_online()
